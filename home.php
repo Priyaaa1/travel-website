f9fb35bde875e1a889f2c7a4edaee29381ea8f5b
Priya <priyamaheshwari183@gmail.com>
2024-07-01 01:37:18 +0530

    added footer

--- home.php
+++ home.php
@@ -15,18 +15,60 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+    <!-- Header section -->
     <section class="header">
         <a href="home.php" class="logo">Travel</a>
 
         <nav class="navbar">
             <a href="home.php">Home</a>
-            <a href="home.php">About</a>
-            <a href="home.php">Package</a>
-            <a href="home.php">Book</a>
+            <a href="about.php">About</a>
+            <a href="package.php">Package</a>
+            <a href="book.php">Book</a>
         </nav> 
 
         <div id="menu-btn" class="fas fa-bars"></div>
-        
+
+    </section>
+
+    <!-- Footer section -->
+    <section class="footer">
+        <div class="box-container">
+            <div class="box">
+                <h3>Quick Links</h3>
+                <a href="home.php"><i class="fas fa-angle-right"></i>Home</a>
+                <a href="about.php"><i class="fas fa-angle-right"></i>About</a>
+                <a href="package.php"><i class="fas fa-angle-right"></i>Package</a>
+                <a href="book.php"><i class="fas fa-angle-right"></i>Book</a>
+            </div>
+
+            <div class="box">
+                <h3>Extra Links</h3>
+                <a href="#"><i class="fas fa-angle-right"></i>Ask questions</a>
+                <a href="#"><i class="fas fa-angle-right"></i>About us</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Privacy policy</a>
+                <a href="#"><i class="fas fa-angle-right"></i>Terms of use</a>
+            </div>
+
+            <div class="box">
+                <h3>Contact info</h3>
+                <a href="#"><i class="fas fa-phone"></i>+91 1234567890</a>
+                <a href="#"><i class="fas fa-envelope"></i>travel@gmail.com</a>
+                <a href="#"><i class="fas fa-map"></i>Madhya Pradesh, India</a>
+            </div>
+
+            <div class="box">
+                <h3>Follow us</h3>
+                <a href="#"><i class="fab fa-facebook-f"></i>Facebook</a>
+                <a href="#"><i class="fab fa-twitter"></i>Twitter</a>
+                <a href="#"><i class="fab fa-instagram"></i>Instagram</a>
+                <a href="#"><i class="fab fa-linkedin"></i>LinkedIn</a>
+                
+            </div>
+
+        </div>
+
+        <div class="credit">Created by<span> Priya Maheshwari </span>| All rights reserved!</div>
     </section>
 
     <!-- swiper js link -->
