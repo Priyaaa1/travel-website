53c4a12a32e72866c106034505384d7695bae596
Priya <priyamaheshwari183@gmail.com>
2024-06-30 22:33:49 +0530

    initial commit

---
+++ home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+
+    <!-- swiper css link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+
+    <!-- css file -->
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="header">
+        <a href="home.php" class="logo">Travel</a>
+
+        <nav class="navbar">
+            <a href="home.php">Home</a>
+            <a href="home.php">About</a>
+            <a href="home.php">Package</a>
+            <a href="home.php">Book</a>
+        </nav> 
+
+        <div id="menu-btn" class="fas fa-bars"></div>
+        
+    </section>
+
+    <!-- swiper js link -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
+    <!-- js file -->
+    <script src="js/script.js"></script>
+</body>
+</html>
